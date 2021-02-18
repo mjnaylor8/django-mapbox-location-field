68a41346052ade2a6686270388323609f803f872
Michael Naylor <34126775+mjnaylor8@users.noreply.github.com>
2021-02-18 11:40:41 +0000

    Update admin.py

--- mapbox_location_field/spatial/admin.py
+++ mapbox_location_field/spatial/admin.py
@@ -1,13 +1,24 @@
 from .models import SpatialLocationField
 
 from ..admin import MapAdmin
-from ..models import LocationField, AddressAutoHiddenField
-from ..widgets import MapAdminInput, AddressHiddenAdminInput
+from ..models import LocationField, AddressAutoHiddenField, AddressCountryField, \
+    AddressDistrictField, AddressRegionField, AddressLocalityField, \
+    AddressPlaceField, AddressPostcodeField, AddressLineField
+from ..widgets import MapAdminInput, AddressHiddenAdminInput, \
+    AddressCountryAdmin, AddressDistrictAdmin, AddressRegionAdmin, \
+    AddressLocalityAdmin, AddressPlaceAdmin, AddressPostcodeAdmin, AddressLineAdmin
 
 
 class SpatialMapAdmin(MapAdmin):
     formfield_overrides = {
         LocationField: {'widget': MapAdminInput},
         SpatialLocationField: {'widget': MapAdminInput},
-        AddressAutoHiddenField: {"widget": AddressHiddenAdminInput, }
+        AddressAutoHiddenField: {"widget": AddressHiddenAdminInput, },
+        AddressCountryField: {"widget": AddressCountryAdmin, },
+        AddressDistrictField: {"widget": AddressDistrictAdmin, },
+        AddressRegionField: {"widget": AddressRegionAdmin, },
+        AddressLocalityField: {"widget": AddressLocalityAdmin, },
+        AddressPlaceField: {"widget": AddressPlaceAdmin, },
+        AddressPostcodeField: {"widget": AddressPostcodeAdmin, },
+        AddressLineField: {"widget": AddressLineAdmin, },
     }
