793e8638c38e64325c350597a41e14b4e732efa1
Michael Naylor <34126775+mjnaylor8@users.noreply.github.com>
2021-02-18 12:19:51 +0000

    Update forms.py

--- mapbox_location_field/forms.py
+++ mapbox_location_field/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
-from .widgets import MapInput, AddressAutoHiddenInput
+from .widgets import MapInput, AddressAutoHiddenInput, AddressCountry, AddressRegion, \
+    AddressDistrict, AddressPlace, AddressLocality, AddressPostcode, AddressLine
 
 
 def reverse_tuple_string(location_string):
